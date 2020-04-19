652bd3b314f85f248d2ee0afc3fe3fa3244fff46
Cezary Piątek <cezarypiatek@live.com>
2020-04-19 16:10:41 +0200

    Add tests cases for onbuildgenerator

---
+++ MappingGenerator/MappingGenerator/MappingGenerator.Test/OnBuildGenerator/OnBuildGeneratorTest.cs
@@ -0,0 +1,22 @@
+﻿using MappingGenerator.OnBuildGenerator;
+using NUnit.Framework;
+using SmartCodeGenerator.TestKit;
+using static MappingGenerator.Test.OnBuildGenerator.OnBuildGeneratorTestCases;
+
+namespace MappingGenerator.Test.OnBuildGenerator
+{
+    public class OnBuildGeneratorTest
+    {
+        [Test]
+        public void should_be_able_to_generate_mapping_interface_implementation()
+        {
+            var generatorFixture = new SmartCodeGeneratorFixture(typeof(OnBuildMappingGenerator), new[]
+            {
+                ReferenceSource.FromType<MappingInterface>()
+            });
+
+
+            generatorFixture.AssertGeneratedCode(_001_SimpleMappingInterface, _001_SimpleMappingInterface_TRANSFORMED);
+        }
+    }
+}
